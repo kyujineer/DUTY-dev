2003dd5dca47ee0ea58cfd4a515d912bea4d00ab
Q <kjung10@gmail.com>
2020-07-28 21:38:58 +0900

    Activities updated with new functions

--- app/src/main/java/com/example/duty/MainActivity.java
+++ app/src/main/java/com/example/duty/MainActivity.java
@@ -4,19 +4,40 @@ package com.example.duty;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.example.duty.menu.MenuActivity;
+
+import java.io.Serializable;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // Intent 받아오기
+        Intent intent = getIntent();
+        // 유저 생성
+        user = new User(
+                intent.getStringExtra("ID"),
+                intent.getStringExtra("name"),
+                intent.getStringExtra("teamId"),
+                intent.getStringExtra("teamName"),
+                intent.getStringExtra("role"),
+                intent.getBooleanExtra("isAdmin",false)
+        ); // 고칠예정
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
+        // turn page to menu layout
+        intent = new Intent(MainActivity.this, MenuActivity.class);
+        // move to Activity
+        startActivity(intent);
 
     }
 }
