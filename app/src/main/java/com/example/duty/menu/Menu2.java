25bac340d416f9857860756575cf0f8324dc0a21
John-Park-Kor <leebs1991@gmail.com>
2020-08-03 20:09:04 +0900

    달력 기능 추가를 위한 dependencies 추가 및 menu2 호출시 calendar_main layout 호출 및 CalendarView Class 호출

--- app/src/main/java/com/example/duty/menu/Menu2.java
+++ app/src/main/java/com/example/duty/menu/Menu2.java
@@ -1,5 +1,7 @@
 package com.example.duty.menu;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,16 +12,21 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.duty.R;
+import com.example.duty.calendar.CalendarView;
 
 public class Menu2 extends Fragment {
 
-    private View view;
+    View view;
+    CalendarView calendarView;
+    Context context;
 
+    @SuppressLint("ResourceType")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.menu_2, container, false);
+        view = inflater.inflate(R.layout.calendar_main, container, false);
 
         return view;
     }
+
 }
