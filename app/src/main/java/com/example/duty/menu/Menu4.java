35f4283761e65c8daa1cc68329424e38b8801d27
llroad <llroad@naver.com>
2020-08-07 15:34:03 +0900

    Menu1 layout

--- app/src/main/java/com/example/duty/menu/Menu4.java
+++ app/src/main/java/com/example/duty/menu/Menu4.java
@@ -1,255 +1,25 @@
 package com.example.duty.menu;
-// main_menu = popup
 
-import android.Manifest;
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.provider.Settings;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.PopupMenu;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
-import com.example.duty.MainActivity;
 import com.example.duty.R;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 public class Menu4 extends Fragment {
 
-    private static final int MY_PERMISSION_CAMERA = 1111;
-    private static final int REQUEST_TAKE_PHOTO = 2222;
-    private static final int REQUEST_TAKE_ALBUM = 3333;
-    private static final int REQUEST_IMAGE_CROP = 4444;
-
     private View view;
-    private Context context;
-
-    String mCurrentPhotoPath;
-    Uri imageURI;
-    Uri photoURI, albumURI;
-    ImageView user_profile;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        setHasOptionsMenu(true);
         view = inflater.inflate(R.layout.menu_4, container, false);
-        context = container.getContext();
-        user_profile = (ImageView) view.findViewById(R.id.user_profile);
-        user_profile.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                PopupMenu pop = new PopupMenu(getActivity().getApplicationContext(), view);
-                getActivity().getMenuInflater().inflate(R.menu.menu_4_popup, pop.getMenu());
-                pop.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem menuItem) {
-                        switch (menuItem.getItemId()) {
-                            case R.id.pop_one:
-                                captureCamera();
-                                break;
-                            case R.id.pop_two:
-                                getAlbum();
-                                break;
-                        }
-                        return true;
-                    }
-                });
-                pop.show();
-                checkPermission();
-            }
-        });
+
         return view;
     }
-
-
-    private void checkPermission() {
-        if(ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE)!= PackageManager.PERMISSION_GRANTED){
-            if((ActivityCompat.shouldShowRequestPermissionRationale((Activity) context,Manifest.permission.WRITE_EXTERNAL_STORAGE))||
-                    (ActivityCompat.shouldShowRequestPermissionRationale((Activity) context,Manifest.permission.CAMERA))){
-                new AlertDialog.Builder(context).setTitle("알림").setMessage("저장소 권한이 거부되었습니다.").setNeutralButton("설정", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                        Intent intent = new Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
-//                        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(user_profile);
-
-
-                        intent.setData(Uri.parse("package: " + getActivity().getPackageName()));
-                        startActivity(intent);
-                    }
-                }).setPositiveButton("확인", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialogInterface, int i) {
-                       getActivity().finish();
-                    }
-                }).setCancelable(false).create().show();
-            }else{
-                ActivityCompat.requestPermissions((Activity) context,new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA}, MY_PERMISSION_CAMERA);
-            }
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode==0){
-            if(grantResults[0]==0){
-                Toast.makeText(context,"카메라 권한 승인완료",Toast.LENGTH_SHORT).show();
-            }else{
-                Toast.makeText(context,"카메라 권한 승인 거절",Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
-    
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        switch (requestCode){
-            case REQUEST_TAKE_PHOTO:
-                if(resultCode == Activity.RESULT_OK){
-                    try{
-                        Log.i("REQUEST_TAKE_PHOTO","OK!!!!!!");
-                        galleryAddPic();
-                        user_profile.setImageURI(imageURI);
-                    }catch(Exception e){
-                        Log.e("REQUEST_TAKE_PHOTO",e.toString());
-                    }
-                }else{
-                    Toast.makeText(context,"저장공간에 접근할 수 없는 기기 입니다.",Toast.LENGTH_SHORT).show();
-                }
-                break;
-            case REQUEST_TAKE_ALBUM:
-                if(resultCode == Activity.RESULT_OK){
-                    if(data.getData() != null){
-                        try {
-                            File albumFile = null;
-                            albumFile = createImageFile();
-                            photoURI = data.getData();
-                            albumURI = Uri.fromFile(albumFile);
-                            cropImage();
-                        } catch (IOException e) {
-                            Log.e("TAKE_ALBUM_SINLE_ERROR",e.toString());
-                        }
-                    }
-                }
-                break;
-            case REQUEST_IMAGE_CROP:
-                if(resultCode == Activity.RESULT_OK){
-                    galleryAddPic();
-                    //사진 변환 error
-                    user_profile.setImageURI(albumURI);
-                }
-                break;
-        }
-    }
-
-//    @Override
-//    public boolean onOptionsItemSelected(MenuItem item) {
-//        int id = item.getItemId();
-//        if (id == 1) {
-//            return true;
-//        }
-//        return super.onOptionsItemSelected(item);
-//    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    private void captureCamera() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-            if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
-                File photoFile = null;
-                try {
-                    photoFile = createImageFile();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-                if (photoFile != null) {
-                    Uri providerUri = FileProvider.getUriForFile(context, getActivity().getPackageName(), photoFile);
-                    imageURI = providerUri;
-
-                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, providerUri);
-                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
-                }
-            } else {
-                Toast.makeText(context, "접근 불가합니다", Toast.LENGTH_SHORT).show();
-                return;
-            }
-        }
-    }
-
-    private void getAlbum() {
-        Intent intent = new Intent(Intent.ACTION_PICK);
-        intent.setType("image/*");
-        intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
-        startActivityForResult(intent, REQUEST_TAKE_ALBUM);
-    }
-
-
-    private File createImageFile() throws IOException {
-        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
-        String imageFileName = "JPEG_" + timeStamp + ".jpg";
-        File imageFile = null;
-        File storageDir = new File(context.getExternalFilesDir(null), "/Pictures");
-
-        if (!storageDir.exists()) {
-            storageDir.mkdirs();
-        }
-        imageFile = new File(storageDir, imageFileName);
-        mCurrentPhotoPath = imageFile.getAbsolutePath();
-
-        return imageFile;
-    }
-
-
-    public void cropImage() {
-        Intent cropIntent = new Intent("com.android.camera.action.CROP");
-        cropIntent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-        cropIntent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-        cropIntent.setDataAndType(photoURI, "image/*");
-        cropIntent.putExtra("aspectX", 1);
-        cropIntent.putExtra("aspectY", 1);
-        cropIntent.putExtra("scale", true);
-        cropIntent.putExtra("output", albumURI);
-        startActivityForResult(cropIntent, REQUEST_IMAGE_CROP);
-    }
-
-    private void galleryAddPic(){
-        Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        File file = new File(mCurrentPhotoPath);
-        Uri contentURI = Uri.fromFile(file);
-        mediaScanIntent.setData(contentURI);
-        getActivity().sendBroadcast(mediaScanIntent);
-        Toast.makeText(context, "저장 완료", Toast.LENGTH_SHORT).show();
-    }
 }
-
-
