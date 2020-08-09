0a49a96aba15ef75b52c8694509e8b5497474a33
Q <kjung10@gmail.com>
2020-08-09 15:54:33 +0900

    menu3 fragments, layouts, functions added

--- app/src/main/java/com/example/duty/team/TeamPostFragment.java
+++ app/src/main/java/com/example/duty/team/TeamPostFragment.java
@@ -1,40 +1,46 @@
-package com.example.duty.menu;
+package com.example.duty.team;
 
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
 
 import com.example.duty.R;
 
-public class Menu3 extends Fragment {
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TeamPostFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TeamPostFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
-    public Menu3() {
+
+    public TeamPostFragment() {
         // Required empty public constructor
     }
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TeamCalendarFragment.
+     * @return A new instance of fragment TeamPostFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static Menu3 newInstance(String param1, String param2) {
-        Menu3 fragment = new Menu3();
+    public static TeamPostFragment newInstance(String param1, String param2) {
+        TeamPostFragment fragment = new TeamPostFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -51,21 +57,10 @@ public class Menu3 extends Fragment {
         }
     }
 
-    // View Variables
-    ImageView imageView;
-
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.menu_3, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_team_post, container, false);
     }
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
-
 }
-
-
-
