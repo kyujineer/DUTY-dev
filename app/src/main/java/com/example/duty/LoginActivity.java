f8635ffd9e6322e2426d72e81e0ceedbcb20d0a8
John-Park-Kor <leebs1991@gmail.com>
2020-08-09 15:18:54 +0900

    아이디 1, 비번 1로 로그인 가능하게 해줌

--- app/src/main/java/com/example/duty/LoginActivity.java
+++ app/src/main/java/com/example/duty/LoginActivity.java
@@ -61,6 +61,11 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                if (id.getText().toString().equals("1") && pw.getText().toString().equals("1")) {
+                    Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
+                    startActivity(intent);
+                }
+
                 // if isAdmin
                 if (isAdmin.isChecked()) {
                     // check if id is available; if true, login; if false, fail login;
