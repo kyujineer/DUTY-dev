86839ec1e55ebdfd4a18dcf8cdd1a944d159c98b
llroad <llroad@naver.com>
2020-08-12 15:25:35 +0900

    Menu1

--- app/src/main/java/com/example/duty/menu/Menu1_frag3.java
+++ app/src/main/java/com/example/duty/menu/Menu1_frag3.java
@@ -1,4 +1,31 @@
 package com.example.duty.menu;
 
-public class Menu1_frag1 {
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.duty.R;
+
+
+public class Menu1_frag3 extends Fragment {
+    // 후번초 근무자 frag
+    FrameLayout frameLayout;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.menu1_frag3, container, false);
+        frameLayout = (FrameLayout) view.findViewById(R.id.fl_3);
+        frameLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 뒤로 가기
+                getActivity().onBackPressed();
+            }
+        });
+        return view;
+    }
 }
