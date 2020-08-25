b51c2240980273a6bc14e68e4e39f6a694ec2352
John-Park-Kor <leebs1991@gmail.com>
2020-08-25 20:22:07 +0900

    manifest changed

--- app/src/main/java/com/example/duty/team/TeamPrototypeFragment.java
+++ app/src/main/java/com/example/duty/team/TeamPrototypeFragment.java
@@ -1,5 +1,6 @@
 package com.example.duty.team;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.duty.R;
 
@@ -25,6 +27,7 @@ public class TeamPrototypeFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Button btn_request;
 
     public TeamPrototypeFragment() {
         // Required empty public constructor
@@ -55,12 +58,26 @@ public class TeamPrototypeFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_team_prototype, container, false);
+        View view = inflater.inflate(R.layout.fragment_team_prototype, container, false);
+
+        btn_request = (Button) view.findViewById(R.id.btn_request);
+        btn_request.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), TeamCalendarActivity.class);
+
+                startActivity(intent);
+            }
+        });
+
+        return view;
+
     }
 }
